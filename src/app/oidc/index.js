3cce503589044f81024506ecfcf7f62723c60332
AKIRA-MIYAKE <akira.aktk.miyake@gmail.com>
2017-12-11 17:41:01 +0900

    refactor: use async/await

--- src/app/oidc/index.js
+++ src/app/oidc/index.js
@@ -4,42 +4,36 @@ const Provider = require('oidc-provider')
 const { getMountOption } = require('./helpers/get-mount-option')
 const defaultSettings = require('./settings')
 
-const handleRequest = (event, context, callback, settings = defaultSettings) => {
+const handleRequest = async (event, context, callback, settings = defaultSettings) => {
   const oidc = new Provider(settings.issure, settings.configuration)
 
-  oidc.initialize(settings.initialization)
-  .then(() => {
-    oidc.app.proxy = true
-    oidc.app.keys = settings.secureKeys
+  await oidc.initialize(settings.initialization)
+  oidc.app.proxy = true
+  oidc.app.keys = settings.secureKeys
 
-    return oidc.app
-  })
-  .then(app => {
-    const mountOption = getMountOption(event)
+  const mountOption = getMountOption(event)
 
-    let koaApp
-    if (!mountOption) {
-      const logger = require('koa-logger')
+  let koaApp
+  if (!mountOption) {
+    const logger = require('koa-logger')
 
-      koaApp = app
-      koaApp.use(logger())
-    } else {
-      const Koa = require('koa')
-      const logger = require('koa-logger')
-      const mount = require('koa-mount')
+    koaApp = oidc.app
+    koaApp.use(logger())
+  } else {
+    const Koa = require('koa')
+    const logger = require('koa-logger')
+    const mount = require('koa-mount')
 
-      koaApp = new Koa()
-      koaApp.proxy = true
-      koaApp.keys = settings.secureKeys
-      koaApp.use(logger())
-      koaApp.use(mount(mountOption.directory, app))
+    koaApp = new Koa()
+    koaApp.proxy = true
+    koaApp.keys = settings.secureKeys
+    koaApp.use(logger())
+    koaApp.use(mount(mountOption.directory, oidc.app))
 
-      event.path = mountOption.rewriteEventPath
-    }
+    event.path = mountOption.rewriteEventPath
+  }
 
-    serverlessHttp(koaApp)(event, context, callback)
-  })
-  .catch(error => callback(error))
+  serverlessHttp(koaApp)(event, context, callback)
 }
 
 module.exports = { handleRequest }
